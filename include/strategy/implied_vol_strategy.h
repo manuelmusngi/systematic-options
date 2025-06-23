a5babfeac8c364ba5f00b2d997475dabf6d22e7a
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2025-06-23 14:26:44 -0400

    Update implied_vol_strategy.h

--- include/strategy/implied_vol_strategy.h
+++ include/strategy/implied_vol_strategy.h
@@ -1,8 +1,10 @@
+#pragma once
 #ifndef IMPLIED_VOL_STRATEGY_H
 #define IMPLIED_VOL_STRATEGY_H
 
 #include "data/market_data.h"
 #include "data/option_data.h"
+#include "strategy.h"
 #include <vector> // For std::vector
 
 class ImpliedVolStrategy {
@@ -11,8 +13,9 @@ public:
     // option: the option contract to analyze
     // market: current market data for the underlying
     // historical_prices: vector of historical prices for volatility forecasting
-    void analyze(const OptionData& option, const MarketData& market,
-                 const std::vector<double>& historical_prices);
+    void analyze(const OptionData& option, 
+                 const MarketData& market,
+                 const std::vector<double>& historical_prices)override;
 };
 
 #endif // IMPLIED_VOL_STRATEGY_H
